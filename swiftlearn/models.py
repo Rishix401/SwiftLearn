14afa3729a5ea97b0b8959ecbd77f0f41a471f27
Rishi <rishix404@gmail.com>
2023-04-15 12:59:22 +0530

    fixed some bugs

--- swiftlearn/models.py
+++ swiftlearn/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='users'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='users'
+    )
+
--- swiftlearn/views.py
+++ swiftlearn/views.py
@@ -1,7 +1,69 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
 
 from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, "swiftlearn/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "swiftlearn/login.html", {
+                "message": "Invalid email and/or password!"
+            })
+    else:
+        return render(request, "swiftlearn/login.html")
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "swiftlearn/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+        except IntegrityError:
+            return render(request, "swiftlearn/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "swiflearn/register.html")
+
